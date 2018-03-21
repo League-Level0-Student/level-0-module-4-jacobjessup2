cae3d8f04fa888a710ce6b4d68dc9e481c790af0
Jessup.Jacob <Jessup.Jacob@SEMS-259-16.SEMS259.LAB>
2018-03-21 14:59:12 -0700

    FruitQuiz

--- src/FruitQuiz.java
+++ src/FruitQuiz.java
@@ -21,9 +21,11 @@ public class FruitQuiz extends KeyAdapter {
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		// 1. Print out the key code variable
-
+System.out.println(keyCode);
 		// 2. Make 3 int variables that hold the key codes for A, b, and C
-		
+		int A = 65;
+		int B = 66;
+		int C = 67;
 		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
 		
 		// 12. If question2 is showing,
@@ -33,12 +35,15 @@ public class FruitQuiz extends KeyAdapter {
 			
 		if (question1.isShowing()) {
 			// 3. If they selected the right fruit, do steps 4 and 7
+			if(keyCode==67) {
+				
 			
 				// 4. Call the correct() method
+				correct();
 				
 				// 7. Use the nextQuestion() method to go to question2
 			
-			
+			}
 			// 8. else (if they touched something else)
 				
 				// 9. Call the incorrect() method
@@ -52,7 +57,7 @@ public class FruitQuiz extends KeyAdapter {
 		// the default package. It must be a .wav file. 
 		// There are lots on freesound.org
 		// 6. Use the playSound method to play your sound
-
+playSound(String correct);
 
 	}
 
