6b5ff094ec77b96070a4c9380be5add7e02997a9
jessup.jacob <jessup.jacob@SEMS-259-15.SEMS259.LAB>
2018-04-25 14:59:18 -0700

    sleepyhead,string methods 2

--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -9,20 +9,37 @@ public class SleepyHead {
 
     public static void main(String[] args) {
         
-        boolean isWeekday, isVacation;
+        int isWeekday, isVacation = 0;
         
         /*
          * Ask the user for these values using
          * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
          * JOptionPane.YES_NO_OPTION);
          */
-    
+   isWeekday=JOptionPane.showConfirmDialog(null, "Is it a weekday?","",JOptionPane.YES_NO_OPTION);
+System.out.println(isWeekday);
+System.out.println(isVacation);
+   if(isWeekday==1); {
+	  isVacation=JOptionPane.showConfirmDialog(null, "So it's a weekend or vacation?","",JOptionPane.YES_NO_OPTION);
+   }
+
+if(isWeekday==0 && isVacation==1) {
+	JOptionPane.showMessageDialog(null, "Get up lazybones!");
+}
+else if(isVacation==0 && isWeekday==1) {
+	  JOptionPane.showMessageDialog(null, "Sleep in then.");	  
+  }
+else if(isVacation==1 && isWeekday==1) {
+	  JOptionPane.showMessageDialog(null, "WAIT WHAT!?");
+  }
 
 
-        /*
-         * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!” If it is a weekday, and we are on vacation,
-         * print “sleep in”.
+
+
+/*
+         * Print "sleep in" if it is a vacation or a weekend. If it's a weekday,
+         * print â€œget up lazybones!â€� If it is a weekday, and we are on vacation,
+         * print â€œsleep inâ€�.
          */
     }
 }
--- src/StringMethods2.java
+++ src/StringMethods2.java
@@ -7,18 +7,23 @@ public class StringMethods2 {
 		
 		// 1. Create a String variable and initialize it with
 		//    random upper and lower case characters.
-
+String random = "rIcKrOlLeD";
 		// 2. Print your String to the console in upper case.
-		
+		System.out.println(random.toUpperCase());
 		// 3. Print your String to the console in lower case.
-
+		System.out.println(random.toLowerCase());
 		// 4. Print the first 3 char's of your String
 		//    HINT: .substring(start,end)
-		
+		System.out.println(random.substring(0,3));
 		// 5. Print a single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
-		
+		System.out.println(random.charAt(5));
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
+	System.out.println(random.length());
+	
+	System.out.println(random.charAt(7));
+	System.out.println(random.charAt(8));
+	System.out.println(random.charAt(9));
 	}
 }
